d69d59fe0234bdaeeb90aaeca8c586a127acb1be
mdmoinulhossain <mdmoinulhossain100@gmail.com>
2023-08-02 17:26:21 +0600

    array testing

--- Study/Basic/Array.php
+++ Study/Basic/Array.php
@@ -1,4 +1,48 @@
 <?php
+# An array is a special variable, which can hold more than one value at a time.
+/*
+* Indexed arrays - Arrays with a numeric index
+* Associative arrays - Arrays with named keys
+* Multidimensional arrays - Arrays containing one or more arrays
+*/
+// Get The Length of an Array - The count() Function
+
+// **************************************************************************** Indexed Arrays
+$fruits = ["apple", "banana", "orange"]; // Create indexed array called fruits
+echo $fruits[2] . "<br/>"; // Output: orange
+// Loop Through an Indexed Array
+/*
+foreach ($fruits as &$fruit) {
+    echo "$fruit\n";
+}
+*/
+
+$arrlength = count($fruits);
+
+for($x = 0; $x < $arrlength; $x++) {
+  echo "$fruits[$x]\n";
+}
+echo "<br/>";
+
+// **************************************************************************** Associative Arrays
+# Associative arrays are arrays that use named keys(key => value) that you assign to them.
+$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+// or
+$age['Peter'] = "35";
+$age['Ben'] = "37";
+$age['Joe'] = "43";
+
+echo $age['Ben']. "<br/>"; // Output: 37
+
+foreach($age as $key => $value){
+ echo "\n$key" . "=" . "$value";
+}
+
+
+
+
+
+// **************************************************************************** Multidimensional Arrays
 
 
 ?>
