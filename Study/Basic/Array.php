2a93f3fffd960de872a1c80351f97f9e58ba5f9f
mdmoinulhossain <mdmoinulhossain100@gmail.com>
2023-08-06 16:03:36 +0600

    update

--- Study/Basic/Array.php
+++ Study/Basic/Array.php
@@ -88,4 +88,17 @@ $threeDimensionalArray = array(
 );
 
 print_r($threeDimensionalArray);
+
+
+
+
+// **************************** Array Sort
+/*
+sort() - sort arrays in ascending order
+rsort() - sort arrays in descending order
+asort() - sort associative arrays in ascending order, according to the value
+ksort() - sort associative arrays in ascending order, according to the key
+arsort() - sort associative arrays in descending order, according to the value
+krsort() - sort associative arrays in descending order, according to the key
+*/
 ?>
