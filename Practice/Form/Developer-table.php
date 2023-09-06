e49dd2f03cc0d3bb439b040aedbf0ba2fe207023
mdmoinulhossain <mdmoinulhossain100@gmail.com>
2023-09-06 14:52:09 +0600

    without database connection

--- Practice/Form/Developer-table.php
+++ Practice/Form/Developer-table.php
@@ -15,42 +15,62 @@
         echo "Connection Error From Database";
     }
 
+    $emErr = $nmErr = $wsErr = $cmErr = "";
+    $em = $nm = $ws = $cm = "";
 
-    if (isset($_POST['submit'])) {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-        $em = $_POST['email'];
-        $nm = $_POST['name'];
-        $ws = $_POST['website'];
-        $cm = $_POST['comment'];
+
+
+        if (empty($_POST["name"])) {
+            $nmErr = "Name is required";
+        } else {
+            $nm = $_POST["name"];
+        }
+
+        if (empty($_POST["email"])) {
+            $emErr = "Email is required";
+        } else {
+            $em = $_POST['email'];
+        }
+
+        if (empty($_POST["website"])) {
+            $wsErr = "Website is required";
+        } else {
+            $ws = $_POST['website'];
+        }
+
+        if (empty($_POST["comment"])) {
+            $cmErr = "Comment is required";
+        } else {
+            $cm = $_POST['comment'];
+        }
+
+
+
 
         $insert = "INSERT INTO `test-data`(`email`, `name`, `website`, `comments`) VALUES ('$em', '$nm', '$ws', '$cm')";
 
         $insertQuery = mysqli_query($connect_db, $insert);
 
-        // Prevent re-submitting data
-        header('location: Developer-table.php');
+        // Prevent re-submitting data, (to redirect page it's self)
+        header("location: " . $_SERVER['PHP_SELF']);
+        exit;
     }
+
 
     ?>
     <h2>Enter your Info.</h2>
     <form method="post" action='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>'>
-        Name: <input type="text" name="name">
+        Name: <input type="text" name="name"> <span><?php echo $nmErr ?></span>
         <br><br>
-        E-mail: <input type="text" name="email">
+        E-mail: <input type="text" name="email"> <span><?php echo $emErr ?></span>
         <br><br>
-        Website: <input type="text" name="website">
+        Website: <input type="text" name="website"> <span><?php echo $wsErr ?></span>
         <br><br>
-        Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+        Comment: <textarea name="comment" rows="5" cols="40"></textarea> <span><?php echo $cmErr ?></span>
         <br><br>
-        <input type="submit" name="submit" value="Submit">
-
-        <?php
-        if (isset($_POST['submit']) == true) {
-        ?>
-            <h1>Success</h1>
-        <?php
-        }
-        ?>
+        <input type="submit" name="submit" value="Submit" id="submitBtn">
     </form>
 </body>
 
