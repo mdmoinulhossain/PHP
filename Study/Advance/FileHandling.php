86b2c2ea13047e7663cb4f2166bef84e88379d6c
mdmoinulhossain <mdmoinulhossain100@gmail.com>
2023-09-11 13:20:34 +0600

    update file

--- Study/Advance/FileHandling.php
+++ Study/Advance/FileHandling.php
@@ -1,3 +1,5 @@
+<!-- PHP FileSystem Details -->
+<!-- https://www.w3schools.com/php/php_ref_filesystem.asp -->
 <?php
 // echo readfile("./function.txt");
 $myfile =  fopen("function.txt", "r") or die("Unable to open file!");
