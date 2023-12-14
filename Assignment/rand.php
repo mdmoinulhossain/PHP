f28b3f66b0967055cb42bd049eda63f4d263381f
MOHAMMAD MOINUL HOSSAIN <57934796+mdmoinulhossain@users.noreply.github.com>
2023-12-14 18:17:13 +0600

    assignment task 1

---
+++ Assignment/rand.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Random quotation change</title>
+    <style>
+        blockquote {
+            position: absolute;
+            top: 20%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            border: 2px dashed #000;
+            padding: 15px;
+            font-size: 20px;
+            font-weight: bold;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    // Array of data
+    $quotes1 = "<blockquote>
+                    সফল মানুষেরা কাজ করে যায়। তারা ভুল করে, ভুল শোধরায় – কিন্তু কখনও হাল ছাড়ে না
+                    <br/>
+                    <small>- কনরাড হিলটন<small>
+                </blockquote>";
+    $quotes2 = "<blockquote>
+                    নিজের প্রতি বিশ্বাস রাখো! নিজের যোগ্যতার ওপর ভরসা রাখো! নিজের শক্তির ওপর বিনয়ী কিন্তু যথেষ্ঠ আস্থা ছাড়া তুমি সফল বা সুখী হতে পারবে না
+                    <br/>
+                <small>- নরম্যান ভিনসেন্ট পীল<small>
+                </blockquote>";
+    $quotes3 = "<blockquote>
+                    পানির গভীরতা নাকের কাছে উঠে আসার আগেই সাঁতার শিখে নাও
+                    <br/>
+                <small>- ড্যানিশ প্রবাদ<small>
+                </blockquote>";
+    $quotes4 = "<blockquote>
+                নেকড়ের পালের সাথে বসবাস করো, তুমি বিড়াল হলেও একদিন গর্জন করতে শিখবে
+                <br/>
+            <small>- ড্যানিশ প্রবাদ<small>
+            </blockquote>";
+    $quotes5 = "<blockquote>
+                অতীত নিয়ে সবসময়ে পড়ে থাকলে তোমার এক চোখ অন্ধ; অতীতকে ভুলে গেলে তোমার দুই চোখই অন্ধ
+                <br/>
+            <small>- রাশিয়ান প্রবাদ<small>
+            </blockquote>";
+
+    $data = array(
+        $quotes1,
+        $quotes2,
+        $quotes3,
+        $quotes4,
+        $quotes5
+    );
+
+    // Get a random index from the array
+    $randomIndex = array_rand($data);
+
+    // Display the data corresponding to the random index
+    echo $data[$randomIndex];
+    ?>
+</body>
+
+</html>
